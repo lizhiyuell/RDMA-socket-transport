62fbfd670a1f6ab62f87065fa7397b4f3e6655d8
lizhiyue <sldjlzy@163.com>
2020-03-09 11:16:56 +0800

    multiple-send thread

--- client.cpp
+++ client.cpp
@@ -5,24 +5,16 @@ int main(){
 
     std::cout<<"This is the client side"<<std::endl;
     char remote_addr1[40] = "tcp://172.23.12.124:8888";
-    // char remote_addr2[40] = "tcp://172.23.12.124:4444";
     char msg1[40] = "This is the client side1";
-    // char msg2[40] = "This is the client side2";
     class rdma::socket client_sock1 = rdma::socket(5);
-    // class rdma::socket client_sock2 = rdma::socket(5);
     client_sock1.connect(remote_addr1);
-    // client_sock2.connect(remote_addr2);
-    // sleep(5);
     int rc;
-    rc = -1;
-    while(rc<0){
-        rc = client_sock1.send(msg1, 40);
-        // std::cout<<"rc is "<<rc<<std::endl;
-    }
-    std::cout<<"send success!"<<std::endl;
     // rc = -1;
-    // while(rc<0) rc = client_sock2.send(msg2, 40);
-    std::cout<<"message send success"<<std::endl;
+    // while(rc<0){
+    //     rc = client_sock1.send(msg1, 40);
+    // }
+    // std::cout<<"send success!"<<std::endl;
+    // std::cout<<"message send success"<<std::endl;
     rc = 0;
     sleep(5);
     while(rc<=0) rc = client_sock1.recv(msg1, 40);
