01684fc8aac7c8c343ed587562a71c5efd1110bf
lizhiyue <sldjlzy@163.com>
2020-03-19 16:24:19 +0800

    debug

--- rdma.cpp
+++ rdma.cpp
@@ -534,7 +534,7 @@ if (rc) {
 		swr.sg_list = &sge;
 		swr.send_flags = IBV_SEND_SIGNALED;
 		swr.num_sge = 1;
-		swr.wr.rdma.remote_addr = (uintptr_t)rrdma->memgt->peer_mr.addr;
+		swr.wr.rdma.remote_addr = (uintptr_t)rrdma->memgt->peer_mr.addr+1;
 		swr.wr.rdma.rkey = rrdma->memgt->peer_mr.rkey;
 
         int index;
