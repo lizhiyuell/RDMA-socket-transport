380a87783bf3d28e25fe73299287143be63b7f7e
lizhiyue <sldjlzy@163.com>
2020-03-22 18:33:46 +0800

    add output

--- rdma.h
+++ rdma.h
@@ -24,7 +24,7 @@
 #define TEST_NZ(x) assert(!x)
 #define __polling
 #define BufferSize 16  // send/recv  size for each node
-#define MAX_CQ_NUM cq_size*2
+#define MAX_CQ_NUM 600
 // RDMA definition
 
 namespace rdma{
