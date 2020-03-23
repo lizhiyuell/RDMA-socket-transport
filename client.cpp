3d0e6730c5e27068168141b9fce55742eac2eb4e
lizhiyue <sldjlzy@163.com>
2020-03-23 21:47:15 +0800

    modify buf size

--- client.cpp
+++ client.cpp
@@ -43,8 +43,8 @@ int main(){
         while(rc<=0) rc = client_sock1.recv(msg1, BufferSize, 0);
         for(int k=0;k<rc;k++){
             memcpy(&count, msg1+k*BufferSize, sizeof(int));
-            if(count==(10000-1)) return 0;
-            // if(count%100==0) 
+            if(count==(100000-1)) return 0;
+            if(count%1000==0) 
             fprintf(stdout, "success with %d\n", count);
             if(count==temp){
                 temp++;
