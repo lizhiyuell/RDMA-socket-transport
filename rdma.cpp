6da823916d2934cfac32dbf342cd7a20a4e16994
lizhiyue <sldjlzy@163.com>
2020-05-16 23:13:03 +0800

    modify simple rdma transport

--- rdma.cpp
+++ rdma.cpp
@@ -143,8 +143,8 @@ using namespace rdma;
         // before seperate addr
         fprintf(stdout, "[Debug] before seperate func, %s\n", addr);
         seperate_addr(addr, ip_addr, bind_port);
-        // sock_port = bind_port;
-        // strcpy(sock_addr, ip_addr);
+        sock_port = bind_port;
+        strcpy(sock_addr, ip_addr);
         // // fprintf(stdout, "[Debug] in inner func of %s: after sperate addr\n", addr);
         // fprintf(stdout, "seperate port number is %d\n", bind_port);
         // fprintf(stdout, "[Debug] in inner func of %s: bind port is %d\n", addr, bind_port);
