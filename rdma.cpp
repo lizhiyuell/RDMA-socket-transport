6c4f6bb9321ca0bfb805eb31883d18a8818af600
lizhiyue <sldjlzy@163.com>
2020-05-16 22:49:52 +0800

    modify simple rdma transport

--- rdma.cpp
+++ rdma.cpp
@@ -19,58 +19,58 @@ using namespace rdma;
         rrdma->memgt = (struct memory_management *) malloc(sizeof( struct memory_management ));
         rrdma->send_count = 0;
 
-        // struct ibv_device **dev_list = NULL;
-        // int num_devices;        
-        // // struct ibv_qp_init_attr qp_init_attr;
-        // struct ibv_device *ib_dev = NULL;
-        // // size_t 	size;
+        struct ibv_device **dev_list = NULL;
+        int num_devices;        
+        // struct ibv_qp_init_attr qp_init_attr;
+        struct ibv_device *ib_dev = NULL;
+        // size_t 	size;
         
-        // // int mr_flags = 0;
-        // // int cq_size = 0;
+        // int mr_flags = 0;
+        // int cq_size = 0;
         
-        // char *dev_name = NULL;
+        char *dev_name = NULL;
 
-        // dev_list = ibv_get_device_list(&num_devices);
-        // if (!dev_list) {
-        //     fprintf(stderr, "failed to get IB devices list\n");
-        //     assert(false);
-        // }
+        dev_list = ibv_get_device_list(&num_devices);
+        if (!dev_list) {
+            fprintf(stderr, "failed to get IB devices list\n");
+            assert(false);
+        }
 
-        // /* if there isn't any IB device in host */
-        // if (!num_devices) {
-        //     fprintf(stderr, "found %d device(s)\n", num_devices);
-        //     assert(false);
-        // }
+        /* if there isn't any IB device in host */
+        if (!num_devices) {
+            fprintf(stderr, "found %d device(s)\n", num_devices);
+            assert(false);
+        }
 
-        // // fprintf(stdout, "found %d device(s)\n", num_devices);
+        // fprintf(stdout, "found %d device(s)\n", num_devices);
     
-        // int device_index;
-        // for (device_index = 0; device_index < num_devices; device_index ++) {
-        //     ib_dev = dev_list[device_index];
+        int device_index;
+        for (device_index = 0; device_index < num_devices; device_index ++) {
+            ib_dev = dev_list[device_index];
 
-        //     /* get device handle */
-        //     rrdma->s_ctx->ctx = ibv_open_device(ib_dev);
-        //     if (!rrdma->s_ctx->ctx) {
-        //         fprintf(stderr, "failed to open device %s\n", dev_name);
-        //         continue;
-        //     }
+            /* get device handle */
+            rrdma->s_ctx->ctx = ibv_open_device(ib_dev);
+            if (!rrdma->s_ctx->ctx) {
+                fprintf(stderr, "failed to open device %s\n", dev_name);
+                continue;
+            }
 
-        //     /* query port properties  */
-        //     if (ibv_query_port(rrdma->s_ctx->ctx, ib_port, &rrdma->s_ctx->port_attr)) {
-        //         fprintf(stderr, "ibv_query_port on port %u failed\n", ib_port);
-        //         continue;
-        //     }
-        //     // fprintf(stdout, "%s phy %d\n", ibv_get_device_name(dev_list[device_index]), rrdma->s_ctx->port_attr.phys_state);
-        //     if( rrdma->s_ctx->port_attr.state == IBV_PORT_ACTIVE ){
-        //         break;
-        //     }
-        // }
+            /* query port properties  */
+            if (ibv_query_port(rrdma->s_ctx->ctx, ib_port, &rrdma->s_ctx->port_attr)) {
+                fprintf(stderr, "ibv_query_port on port %u failed\n", ib_port);
+                continue;
+            }
+            // fprintf(stdout, "%s phy %d\n", ibv_get_device_name(dev_list[device_index]), rrdma->s_ctx->port_attr.phys_state);
+            if( rrdma->s_ctx->port_attr.state == IBV_PORT_ACTIVE ){
+                break;
+            }
+        }
 
 
-        // //	/* We are now done with device list, free it */
-        // // ibv_free_device_list(dev_list);
-        // // dev_list = NULL;
-        // // ib_dev = NULL;
+        // /* We are now done with device list, free it */
+        // ibv_free_device_list(dev_list);
+        // dev_list = NULL;
+        // ib_dev = NULL;
 
         // /* query port properties  */
         // if (ibv_query_gid(rrdma->s_ctx->ctx, ib_port, gid, &rrdma->s_ctx->gid)) {
