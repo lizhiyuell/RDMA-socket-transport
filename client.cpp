d7a66ae3755b540127965304ca680c7fbdad9a0f
lizhiyue <sldjlzy@163.com>
2020-05-20 22:40:21 +0800

    test RDMA two sided

--- client.cpp
+++ client.cpp
@@ -9,8 +9,8 @@
 
 char msg_s[msg_size];
 char msg_r[BufferSize * MAX_CQ_NUM];
-class rdma::socket sock_send;
-class rdma::socket sock_recv;
+class rdma::socket sock_send = rdma::socket(5);
+class rdma::socket sock_recv = rdma::socket(5);
 
 long int get_time(){
     struct timespec c_time;
@@ -48,14 +48,12 @@ int main(){
     char remote_addr1[40] = "tcp://172.23.12.124:8888";
     char remote_addr2[40] = "tcp://172.23.12.124:9999";
     char msg1[BufferSize * MAX_CQ_NUM] = "This is the client side1";
-    sock_send = rdma::socket(3);
-    sock_recv = rdma::socket(3);
     sock_recv.connect(remote_addr1);
     sock_send.connect(remote_addr2);
     printf("connect started\n");
     int rc = 0;
     while(rc<=0) rc = sock_recv.recv(msg_r, 5, 0);
-    printf("pre recv success\n");
+    printf("pre recv\n");
     if(msg_r[0]=='A'&&msg_r[1]=='C'&&msg_r[2]=='K'){
         printf("connection built\n");
         rc=0;
